720d1008cf3b1cc521408a21f7ae7172216e62a5
Richard <rgray@td.com.au>
2016-06-01 10:16:43 +1000

    Working Alpha

---
+++ ExampleScriptProcessing.ps1
@@ -0,0 +1,85 @@
+﻿Param(
+    [string]$Package=(Read-Host "Enter Package Name"),
+    [string]$Offline=$false,
+    [string]$AccessKey="",
+    [string]$SecretKey="",
+    [string]$AWSBucket=""
+)
+#Import PSDA.Gathering module
+Import-Module .\PSDA.Processing.psm1
+
+#ScriptName is used to define the product and binds this script to the data-processing script (alpha numeric only, no spaces), eg, VMwareVCenter, EMCUnity, MSExchange, CiscoUCS
+$ScriptName = "ExampleScriptProcessing"
+$PreviousScript = "ExampleScriptGathering"
+
+
+Get-Package -Package $Package -Offline $Offline -PreviousScript $PreviousScript -ScriptName $ScriptName -AccessKey $AccessKey -SecretKey $SecretKey -AWSBucket $AWSBucket
+New-Report -ScriptName $ScriptName -ClientName $ClientName
+$PackageDirectory = (Get-Location).Path + "\$Package\"
+
+#Run commandlets and pipe them to Add-<content type> for use in the Data-Presentation script
+"Documentation Demo 1" | Add-Title -Level 1
+"Overview 2" | Add-Title -Level 2
+"Tims Asbuilt 3" | Add-Title -Level 3
+"Heading 1" | Add-Title -Level 1
+"Heading 2" | Add-Title -Level 2
+"Secondy Heading 2" | Add-Title -Level 2
+"Heading 3" | Add-Title -Level 3
+"Secondy Heading 3" | Add-Title -Level 3
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante 
+dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce
+nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu
+ad litora torquent per conubia nostra, per inceptos himenaeos.
+ " | Add-Paragraph -Title "Paragraph Demo"-Level 2
+
+#Using a normal Table
+Import-Clixml -Path ($PackageDirectory + "get-process") | Select ProcessName, Handles, PM`(K`), CPU`(s`), Id -first 5| Add-Table -Title "Processes Listing" -Caption "Processes Listing Example" -Level 2 -Description "This is a table showing a list of the running processes"
+Import-Clixml -Path ($PackageDirectory + "get-childitem") | Select Name, Mode -first 5 | Add-Table -Title "Directory Listing" -Caption "Directory Listing Example" -Level 2 -Description "This is a table showing the files in a directory"
+#Using a Vertical Table
+$windows = Import-Clixml -Path ($PackageDirectory + "windows-info")
+$windowsvtable = @{}
+$windowsvtable.Add("Operating System Version",$windows.Version)
+$windowsvtable.Add("Build Number",$windows.BuildNumber)
+$windowsvtable.Add("Serial Number",$windows.SerialNumber)
+$windowsvtable | Add-VerticalTable -Title "Windows Information" -Level 1 -Caption "Windows Information" -Description "The following table details the operating systems information"
+
+$drives = Import-Clixml -Path ($PackageDirectory + "drive-info")
+$drives | % {
+    $drivesvtable = @{}
+    $drivesvtable.Add("Drive",$_.DeviceId)
+    $drivesvtable.Add("Free Space (GB)",([int64]$_.FreeSpace/1GB))
+    $drivesvtable.Add("Drive Size (MB)",([int64]$_.Size/1GB))
+    $drivesvtable | Add-VerticalTable -Title ($_.DeviceId + " Drive Information") -Level 2 -Caption ($_.DeviceId + " Information") -Description ("The following table details the " + $_.DeviceId + " drive  information")
+}
+
+"Code" | Add-Title -Level 2
+ '<html>
+    <head>
+        <title>Demo Title</title>
+    </head>
+	<body>
+		<h1>Heading 1</h1>
+		<p>This is a Paragraph</p>
+	</body>
+</html>' | Add-Code -Title "HTML" -Level 3 -Caption "HTML Example" -Description "The following code is a snippit of HTML." -Language "HTML"
+
+'{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}
+'| Add-Code -Title "Json" -Level 3 -Caption "Json Example" -Description "The following code is a snippit of Json code "
+
+$states = @{"Victoria" = "Melbourne"; "New South Wales" = "Sydney"; "Western Australia" = "Perth"}
+$states.Add("South Australia", "Adelaide")
+$states | Add-VerticalTable -Title "States and Capital Cities" -Level 2 -Caption "Capitals" -Description "The following table maps States and their Capitals"
+
+
+#Run the Submit-Report fuction once at the end of the script body to upload to S3 (and in future trigger cloud processing)
+$ReportName = Submit-Report -AccessKey $AccessKey -SecretKey $SecretKey -AWSBucket $AWSBucket -Offline $Offline -RemoveDirectory $false
---
+++ PSDA.Processing.psm1
@@ -0,0 +1,309 @@
+﻿#Requires -Version 3.0
+#Requires -Modules AWSPowerShell
+
+#region Script Help
+
+<#
+.SYNOPSIS  
+    PowerShell Documentation Automation Script.
+    These cmdlets is used for Processing information from a previously ran PSDA Gathering Script, once processed the output is sent for presentation
+.DESCRIPTION
+    PowerShell Documentation Automation Script.
+    These cmdlets is used for Processing information from a previously ran PSDA Gathering Script, once processed the output is sent for presentation
+.NOTES
+    Version:        1.0
+    Author:         Richard Gray
+.LINK
+    https://github.com/PSAutoDoco/PSAutoDoco
+.EXAMPLE
+ 
+
+
+#>
+
+#endregion
+
+#region script changelog
+
+<#
+.VERSION 1.0 - 27/05/2016
+    Contributors: Richard Gray
+    -Initial Build
+
+
+#>
+
+#endregion
+
+#region Script Cmdlets
+
+#New-Report Creates a new report ready to take input
+Function New-Report {
+    Param(
+        [parameter(Mandatory=$True)][string]$ScriptName,
+        [parameter(Mandatory=$True)][string]$ClientName
+    )
+    #Get ClientName if not set
+    if($ClientName -notmatch "^[a-zA-Z0-9]+$"){
+        Write-Host "Invalid Client Name!" -ForegroundColor Red
+        Write-Host "Alphanumeric only, no spaces or other characters" 
+        do{
+            $ClientName = Read-Host "Enter Client Name"
+            If($ClientName -notmatch  "^[a-zA-Z0-9]+$"){
+                Write-Host "Alphanumeric only, no spaces or other characters"  
+            }  
+        }until($ClientName -ne $null -and $ClientName -match "^[a-zA-Z0-9]+$")
+    }
+    Write-Host "Setting Up Report..." -NoNewline
+    #Get ticks as a time stamp for Package
+    $Script:Ticks = (Get-Date).Ticks
+    #Create the name of the Package using the Client Name, Script Name, and Ticks
+    $Script:ReportName = $ClientName + "-" + $ScriptName + "-" + $Ticks
+    $Script:ReportDirectory = (Get-Location).Path + "\$ReportName"
+    $Script:ReportZip = $Script:ReportDirectory + ".zip"
+    #Create Package Directory
+    $Script:Setup = (New-Item $ReportDirectory -ItemType Directory)
+    "File, Type, Title, Description, Caption, CustomCSS, Level, Language" | Out-file -FilePath "$ReportDirectory\0.csv"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function New-Report
+
+#Submit Report to S3 for archive/processing
+Function Submit-Report {
+    Param(
+        [string]$AccessKey="",
+        [string]$SecretKey="",
+        [string]$AWSBucket="",
+        [string]$Offline=$false,
+        [bool]$RemoveDirectory=$true,
+        [bool]$RemoveZip=$true
+
+    )
+    #Zip the Package Directory
+    Write-Host "Zipping Report..." -NoNewline
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::CreateFromDirectory($ReportDirectory,$ReportZip)
+    Write-Host "Done" -ForegroundColor Green
+    #Upload the Zipped Package to S3
+    Write-Host "Uploading to S3..." -NoNewline
+    If($offline -eq $false){
+        if($AccessKey -eq ""){
+            do{
+                $AccessKey = Read-host "Enter AWS Access Key"
+            }until($AccessKey -ne "")
+        }    
+        if($SecretKey -eq ""){
+            do{
+                $SecretKey = Read-host "Enter AWS Secret Key"
+           } until($SecretKey -ne "")
+        } 
+        if($AWSBucket -eq ""){
+            do{
+                $AWSBucket = Read-host "Enter AWS Bucket Name"
+           } until($AWSBucket -ne "")
+        } 
+        Write-S3Object -BucketName $AWSBucket -File $ReportZip -Key $ReportName -AccessKey $AccessKey -SecretKey $SecrectKey
+        Write-Host "Done" -ForegroundColor Green   
+    }else{
+        Write-Host "Skipping" -ForegroundColor Yellow
+    }
+    Write-host "Removing Directories..." -NoNewline
+    If(($RemovePackageDirectory -eq $true) -or ($Offline -eq $false)){
+        Remove-Item $PackageDirectory -Force -Recurse
+        Remove-Item $ReportDirectory -Force -Recurse
+        Write-host "Done" -ForegroundColor Green
+    }else{
+        Write-Host "Skipping" -ForegroundColor Yellow
+    }    
+    Write-host "Removing Zip files..." -NoNewline
+    If(($RemovePackageZip -eq $true) -or ($Offline -eq $false)){
+        Remove-Item $PackageZip
+        Remove-Item $ReportZip
+        Write-host "Done" -ForegroundColor Green
+    }else{
+        Write-Host "Skipping" -ForegroundColor Yellow
+    }    
+    Write-Host "Report Name [ " -NoNewline
+    Write-Host $ReportName -NoNewline -ForegroundColor Green
+    Write-Host " ]"
+    Write-Host "Script Complete"
+    Return $ReportName
+}
+Export-ModuleMember -Function Submit-Report
+
+#Add Metadata to the Report, do not export this cmdlet
+Function Add-Metadata{
+    Param(
+        [string]$File,
+        [string]$Type,
+        [string]$Title="",
+        [string]$Description="",
+        [string]$Caption="",
+        [string]$CustomCSS="",
+        [string]$Level="",
+        [string]$Language=""
+    )
+    "$File, $Type, $Title, $Description, $Caption, $CustomCSS, $Level, $Language" | Out-File -FilePath "$ReportDirectory\0.csv" -Append
+}
+
+
+#Add Title to the Report
+Function Add-Title{
+    Param(
+        [parameter(ValueFromPipeline=$true)] $Title,
+        [string]$Level=1,
+        [string]$CustomCSS=""
+    )
+    Write-Host "Adding Title..." -NoNewline
+    $script:index++
+    Add-Metadata -File "$index.txt" -Type "Title" -CustomCSS $CustomCSS -Level $Level
+    $Title | Out-File -FilePath "$ReportDirectory\$index.txt"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function Add-Title
+
+#Adds a Paragrah to the Report
+Function Add-Paragraph{
+    Param(
+        [parameter(ValueFromPipeline=$true)] $Paragraph,
+        [string]$Title="",
+        [string]$Level=1,
+        [string]$CustomCSS=""
+    )
+    Write-Host "Adding Paragraph..." -NoNewline
+    $script:index++
+    Add-Metadata -File "$index.txt" -Type "Paragraph" -CustomCSS $CustomCSS -Level $Level -Title $Title
+    $Paragraph | Out-File -FilePath "$ReportDirectory\$index.txt"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function Add-Paragraph
+
+#Adds a Table to the Report
+Function Add-Table{
+    Param(
+        [string]$Title="",
+        [string]$Level=1,
+        [string]$Description="",
+        [string]$Caption="",
+        [string]$CustomCSS=""
+    )    
+    Write-Host "Adding Table..." -NoNewline
+    $script:index++
+    Add-Metadata -File "$index.csv" -Type "Table" -Title $Title -Caption $Caption -CustomCSS $CustomCSS -Level $Level -Description $Description
+    $input | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath "$ReportDirectory\$index.csv"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function Add-Table
+
+#Add a Virtical Table to the Report
+Function Add-VerticalTable{
+    Param(
+        [hashtable][parameter(ValueFromPipeline=$true)] $HashTable,
+        [string]$Title="",
+        [string]$Level=1,
+        [string]$Description="",
+        [string]$Caption="",
+        [string]$CustomCSS=""
+    )    
+    $Table =  $HashTable.getEnumerator() | foreach { new-object -typename psobject -property @{Item = $_.name ; Configuration = $_.value } } | Select Item, Configuration
+    Write-Host "Adding Vertial Table..." -NoNewline
+    $script:index++
+    Add-Metadata -File "$index.csv" -Type "VerticalTable" -Title $Title -Caption $Caption -CustomCSS $CustomCSS -Level $Level -Description $Description
+    $Table | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath "$ReportDirectory\$index.csv"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function Add-VerticalTable
+
+#Adds a code snippit to the Report
+Function Add-Code{
+    Param(
+        [parameter(ValueFromPipeline=$true)] $Code,
+        [string]$Title="",
+        [string]$Level=1,
+        [string]$Description="",
+        [string]$Caption="",
+        [string]$Language="",
+        [string]$CustomCSS=""
+    )    
+    Write-Host "Adding Code..." -NoNewline
+    $script:index++
+    Add-Metadata -File "$index.txt" -Type "Code" -Title $Title -Level $Level -Caption $Caption -CustomCSS $CustomCSS -Language $Language -Description $Description
+    $Code | Out-File -FilePath "$ReportDirectory\$index.txt"
+    Write-Host "Done" -ForegroundColor Green
+}
+Export-ModuleMember -Function Add-Code
+
+#This section needs to be cleaned up
+function Get-Package{
+    Param(
+        [string]$Package="",   
+        [string]$AccessKey="",
+        [string]$SecretKey="",
+        [string]$AWSBucket="",
+        [parameter(Mandatory=$True)][string]$ScriptName="",
+        [string]$PreviousScript="",
+        [string]$Offline=$false
+    )  
+    #Set Console Window Title
+    $WindowTitle = (Get-Host).UI.RawUI
+    $WindowTitle.WindowTitle = "$ScriptName Script" 
+    Write-Host "$ScriptName-Procesing Script" -ForegroundColor Magenta
+    write-host "https://github.com/PSAutoDoco/PSAutoDoco" 
+    $Path = (Get-Location).Path 
+    $Ticks = (Get-Date).Ticks
+    If($Offline -eq $false){
+        if($AccessKey -eq ""){
+            do{
+                $AccessKey = Read-host "Enter AWS Access Key"
+            }until($AccessKey -ne "")
+        }    
+        if($SecretKey -eq ""){
+            do{
+                $SecretKey = Read-host "Enter AWS Secret Key"
+           } until($SecretKey -ne "")
+        } 
+        if($AWSBucket -eq ""){
+            do{
+                $AWSBucket = Read-host "Enter AWS Bucket Name"
+           } until($AWSBucket -ne "")
+        } 
+        If($Package -eq ""){
+            $x=0
+            $Packages=@{}
+            Get-S3Object -BucketName $AWSBucket -AccessKey $AccessKey -SecretKey $SecretKey  | Where-Object {
+                $_.Key.Split("-")[1] -eq $ChildScript
+            } |  % {
+                $x++;
+                $Packages.Set_item($x,$_.key)
+                write-host "[$x]" ($_.key.split("-")[0]) ([datetime][long]$_.key.split("-")[2]) $_.key.split("-")[2]
+            }
+            $z = Read-Host "Pick a Package Number"
+            $Package = $Packages.item([int]$z)
+        }
+        $PackageDirectory = $Path + "\" + $Package
+        $PackageZip = $Package + ".zip"
+        $PackageZipDirectory = $Path + "\" + $PackageZip
+        $Report = $ClientName + "-" + $ScriptName + "-" + $Ticks
+        $ReportDirectory = $Path + "\" + $Report
+        $ReportZip = $ReportDirectory + ".zip"
+        Read-S3Object -BucketName $AWSBucket -Key $Package -File $PackageZip -AccessKey $AccessKey -SecretKey $SecretKey
+        Add-Type -assembly "system.io.compression.filesystem"
+        [io.compression.zipfile]::ExtractToDirectory($PackageZipDirectory,$PackageDirectory)
+    }else{
+        Write-Host "OFFLINE MODE" -ForegroundColor Magenta
+        #offline testing
+        $PackageDirectory = $Path + "\" + $Package
+        $PackageZip = $Package + ".zip"
+        $PackageZipDirectory = $Path + "\" + $PackageZip
+        $Script:ClientName = ($Package.split("-")[0])
+        $Report = $ClientName + "-" + $ScriptName + "-" + $Ticks
+        $ReportDirectory = $Path + "\" + $Report
+        $ReportZip = $ReportDirectory + ".zip"
+    }
+}
+Export-ModuleMember -Function Get-Package
+
+
+
+
+#endregion
